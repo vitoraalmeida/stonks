7e79382b535acceff6ab1ee71487c8c7ec6ea2cd
Vitor <pro.vitoralmeida@gmail.com>
2023-10-13 14:21:21 -0300

    Adiciona o módulo pytest, testes unitários para funções e classes helper, testes funcionais para respostas a requisições https

---
+++ tests/functional/test_stocks.py
@@ -0,0 +1,79 @@
+"""
+Este arquivo (test_stocks.py) contém os testes funcionais para o arquivo app.py
+"""
+from stonks.app import app
+
+
+def test_index_page():
+    """
+    DADA uma aplicação Flask 
+    QUANDO a página '/' for requisitada (GET)
+    ENTÃO checa se a resposta é valida
+    """
+    # flask provê um cliente http para facilitar os testes
+    # que usa uma versão para testes da nossa aplicação 
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b'Stonks - Monitore e gerencie seus investimentos' in response.data
+        assert b'Bem vindo ao Stonks!' in response.data
+
+
+def test_about_page():
+    """
+    DADA uma aplicação Flask 
+    QUANDO a página '/about' for requisitada (GET)
+    ENTÃO checa se a resposta é valida
+    """
+    with app.test_client() as client:
+        response = client.get('/about')
+        assert response.status_code == 200
+        assert b'Stonks - Monitore e gerencie seus investimentos' in response.data
+        assert b'Sobre' in response.data
+        assert 'Essa aplicação foi feita utilizando o framework web Flask' in response.data.decode()
+        assert b'Desenvolvido por Vitor' in response.data
+
+
+def test_get_add_stock_page():
+    """
+    DADA uma aplicação Flask 
+    Quando a página '/add_stock' for requisitada (GET)
+    ENTÃO checa se a resposta é valida
+    """
+    with app.test_client() as client:
+        response = client.get('/add_stock')
+        assert response.status_code == 200
+        assert b'Stonks - Monitore e gerencie seus investimentos' in response.data
+        assert 'Adicione uma ação' in response.data.decode()
+        assert 'Ticket: <em>(obrigatório, de 1-5 letras maiúsculas</em>' in response.data.decode()
+        assert 'Número de ações: <em>(obrigatório)</em>' in response.data.decode()
+        assert 'Preço de compra (R$):' in response.data.decode()
+
+
+def test_post_add_stock_page():
+    """
+    DADA uma aplicação Flask 
+    QUANDO a página '/add_stock' for requisitada (POST)
+    ENTÃO checa se o usuário é redirecionado para a página '/stocks'
+    """
+
+    with app.test_client() as client:
+        response = client.post(
+            '/add_stock',
+            data={
+                'stock_symbol': 'AAPL',
+                'number_of_shares': '23',
+                'purchase_price': '432.17'
+            },
+            follow_redirects=True
+        )
+
+        assert response.status_code == 200
+        assert 'Lista de ações' in response.data.decode()
+        assert b'Ticket' in response.data
+        assert 'Número de ações' in response.data.decode()
+        assert 'Preço de compra' in response.data.decode()
+        assert b'AAPL' in response.data
+        assert b'23' in response.data
+        assert b'432.17' in response.data
+
---
+++ tests/unit/test_app.py
@@ -0,0 +1,89 @@
+from pydantic import ValidationError
+import pytest
+
+"""
+Este arquivo (test_app.py) contém os testes unitários para o arquivo stonks/app.py
+"""
+from stonks.app import StockModel
+
+def test_validate_stock_data_nominal():
+    """
+    DADA uma classe helper para validar dados de formulários
+    QUANDO dados válidos são passados
+    ENTÃO checa se a validação é bem sucedida
+    """
+    stock_data = StockModel(
+        stock_symbol='SBUX',
+        number_of_shares='100',
+        purchase_price='45.67'
+    )
+    assert stock_data.stock_symbol == 'SBUX'
+    assert stock_data.number_of_shares == 100
+    assert stock_data.purchase_price == 45.67
+
+
+def test_validate_stock_data_invalid_stock_symbol():
+    """
+    DADA uma classe helper para validar dados de formulários
+    QUANDO dados inválidos são passados (stock_symbol)
+    ENTÃO checa se a validação invoca um ValueError
+    """
+    with pytest.raises(ValueError):
+        StockModel(
+            stock_symbol='SBUX123',  # Invalid!
+            number_of_shares='100',
+            purchase_price='45.67'
+        )
+
+
+def test_validate_stock_data_invalid_number_of_shares():
+    """
+    DADA uma classe helper para validar dados de formulários
+    QUANDO dados inválidos são passados (number_of_shares)
+    ENTÃO checa se a validação invoca um ValidationError
+    """
+    with pytest.raises(ValidationError):
+        StockModel(
+            stock_symbol='SBUX',
+            number_of_shares='100.1231',  # Invalid!
+            purchase_price='45.67'
+        )
+
+
+def test_validate_stock_data_invalid_purchase_price():
+    """
+    DADA uma classe helper para validar dados de formulários
+    QUANDO dados inválidos são passados (purchase_price)
+    ENTÃO checa se a validação invoca um ValidationError
+    """
+    with pytest.raises(ValidationError):
+        StockModel(
+            stock_symbol='SBUX',
+            number_of_shares='100',
+            purchase_price='45,67'  # Invalid!
+        )
+
+
+def test_validate_stock_data_missing_inputs():
+    """
+    DADA uma classe helper para validar dados de formulários
+    QUANDO dados inválidos são passados (sem input)
+    ENTÃO checa se a validação invoca um ValidationError
+    """
+    with pytest.raises(ValidationError):
+        StockModel()  # Missing input data!
+
+
+def test_validate_stock_data_missing_purchase_price():
+    """
+    DADA uma classe helper para validar dados de formulários
+    QUANDO dados inválidos são passados (sem purchase_price)
+    ENTÃO checa se a validação invoca um ValidationError
+    """
+    with pytest.raises(ValidationError):
+        StockModel(
+            stock_symbol='SBUX',
+            number_of_shares='100',
+            # Missing purchase_price!
+        )
+
